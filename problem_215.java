4803152c429d839db0b0f5b0ad180b396e269fea
dhootraghav <iamraghavdhoot@gmail.com>
2026-02-21 14:00:47 +0000

    solved priority queue returning kth highest array element

---
+++ problem_215.java
@@ -0,0 +1,23 @@
+/*215. Kth Largest Element in an Array
+Given an integer array nums and an integer k, return the kth largest element in the array.
+Note that it is the kth largest element in the sorted order, not the kth distinct element. */
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int num : nums){
+            pq.add(num);
+            if(pq.size() > k){
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+}
+
+/*
+Better solution : (though it was written to do it without sorting)
+
+Arrays.sort(nums);
+return nums[nums.length-k];
+*/
